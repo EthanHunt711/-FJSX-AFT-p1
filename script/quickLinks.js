afb19469f839fc64e04938a62602c9a49f4598e7
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2023-12-29 20:51:27 +0100

    quick link deletion is primarily completed with bug

--- script/quickLinks.js
+++ script/quickLinks.js
@@ -1,10 +1,50 @@
 function quickLinksTile(){
+    // target the quicklinks ul 
     const linkList = document.querySelector('.linkList');
 
+    //create a list element
     const linkElement = document.createElement('li');
     linkElement.className = 'linkListElements';
+    // create a div inside the list elemet
+    const linkElementDiv = linkElement.createElement('div');
+    linkElementDiv.className ='insideDiv';
+
+    linkElement.appendChild(linkElementDiv);
+
+
+    linkList.appendChild(linkElement);
+
 
 
 };
 
-quickLinksTile();
+// quickLinksTile();
+
+
+function deleteQuickLink(){
+    let delIcons = document.getElementsByClassName('deleteIcon');
+    let linkListElemts = document.getElementsByClassName('linkListElemet');
+
+    for (let i = 0; i < delIcons.length; i++){
+        delIcons[i].addEventListener('click', event => {
+            if(linkListElemts[i].style.dispaly === 'block'){
+                linkListElemts[i].style.display = 'none'
+            } else {
+                linkListElemts[i].style.dispaly = 'block'
+            }
+        })
+    }
+};
+
+
+function addQuickLinks(){
+    const addLink = document.querySelector('.addLink')
+    addLink.addEventListener('click', event => {
+        console.log('find me')
+    })
+};
+
+
+
+deleteQuickLink();
+addQuickLinks();
