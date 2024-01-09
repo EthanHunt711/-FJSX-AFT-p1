b54b6f410790328527d5b31e72fe9afec2a6e81f
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2024-01-09 10:53:24 +0100

    popup functions correctly

--- script/quickLinks.js
+++ script/quickLinks.js
@@ -1,50 +1,53 @@
-function quickLinksTile(){
-    // target the quicklinks ul 
-    const linkList = document.querySelector('.linkList');
+//targeting the links div
+const links = document.querySelector('.links');
+//add event listener to each delete icon based on id
+links.addEventListener('click', function(evt){
+    let id = evt.target.id;
+    let divId = id.slice(0, id.length-3);
+    document.getElementById(divId).remove();
+});
 
-    //create a list element
-    const linkElement = document.createElement('li');
-    linkElement.className = 'linkListElements';
-    // create a div inside the list elemet
-    const linkElementDiv = linkElement.createElement('div');
-    linkElementDiv.className ='insideDiv';
+document.querySelector('#onPageAddBtn').addEventListener('click', event => {
+    document.querySelector('.inputForm').style.display = 'flex';
+});
 
-    linkElement.appendChild(linkElementDiv);
+let linkName; 
+let linkAddress; 
 
+document.querySelector('#linkName').addEventListener('input', event => {
+    linkName = document.querySelector('#linkName').value;
+});
 
-    linkList.appendChild(linkElement);
+document.querySelector('#linkAddress').addEventListener('input', event => {
+    linkAddress = document.querySelector('#linkAddress').value;
+});
 
+//function for adding new links
+function addQuickLink(){
+    
 
-
-};
-
-// quickLinksTile();
-
-
-function deleteQuickLink(){
-    let delIcons = document.getElementsByClassName('deleteIcon');
-    let linkListElemts = document.getElementsByClassName('linkListElemet');
-
-    for (var i = 0; i < delIcons.length; i++){
-        delIcons[i].addEventListener('click', event => {
-            if(linkListElemts[i].style.dispaly === 'block'){
-                linkListElemts[i].style.display = 'none'
-            } else {
-                linkListElemts[i].style.dispaly = 'block'
-            }
-        })
-    }
-};
-
-
-function addQuickLinks(){
-    const addLink = document.querySelector('.addLinkBtn')
-    addLink.addEventListener('click', event => {
-        console.log('find me')
-    })
+    //creating the innerHTML which generates a random unique number for id 
+    let newLink = `
+        <li class="linkListElemet" id="${Date.now()}">
+            <div class="insideDiv">
+                <a class="favIcon" href="${linkAddress}"><img src="./icons/links/linkpng.png" alt="Link Icon"></a>
+                <a class="favText" href="${linkAddress}">${linkName}</a>
+                <button class="deleteIcon" id="${Date.now()}Del"><i class="fa-solid fa-circle-minus"></i></button>
+            </div>
+        </li>`;
+    //adding to the existing list
+    document.querySelector('.linkList').innerHTML += newLink;
 };
 
 
-
-deleteQuickLink();
-addQuickLinks();
+document.querySelector('#closeBtnIcon').addEventListener('click', event => {
+    document.querySelector('.inputForm').style.display = 'none';
+    document.querySelector('#linkName').value = "";
+    document.querySelector('#linkAddress').value = "";
+});
+// if(localStorage.ali == undefined){ // Kika om inte finns
+//     localStorage.ali = 'ali'; // Skapa en ny localstorage
+//     alert('First visit'); 
+// }
+// else // Om det finns
+//     alert('Hello Ali');
