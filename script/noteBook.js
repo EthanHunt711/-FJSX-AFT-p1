9c6e0a3184e6d28ee086aedaf93115eabd114cbd
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2024-01-09 09:11:30 +0100

    changes saves in local storage

--- script/noteBook.js
+++ script/noteBook.js
@@ -1,6 +1,9 @@
-// const textAreaContent = document.querySelector('#notebookText');
-
-// textAreaContent.addEventListener('input', event => {
-//     textAreaContent.textContent = textAreaContent + event
-//     console.log(textAreaContent)
-// });
+//text area targeting
+const textAreaContent = document.querySelector('#notebookText');
+//saving the input in the local storage
+textAreaContent.addEventListener('input', () => {
+    localStorage.notebookText = textAreaContent.value;
+});
+//show the previous changes if any exists
+if(localStorage.notebookText != undefined)
+    notebookText.value = localStorage.notebookText;
